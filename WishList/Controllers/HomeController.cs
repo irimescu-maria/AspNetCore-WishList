c44160733e312ee077baece7aa72270861ec099c
irimescu_maria <irimescu.mariaalexandra@gmail.com>
2021-10-11 23:47:05 +0300

    Errors solved

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -9,11 +9,11 @@ namespace WishList.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index(){
-            return View('Index');
+            return View("Index");
         }
 
         public IActionResult Error(){
-            return View('Error');
+            return View("Error");
         }
         
     }
